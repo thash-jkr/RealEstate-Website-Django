64b0750192934222df2ccde894176c551c36a91d
thash-jkr <afthashsahal123@gmail.com>
2022-05-31 20:16:09 +0530

    First model created and migrated

---
+++ listings/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    bedroom = models.IntegerField()
+    bathroom = models.IntegerField()
+    square_feet = models.IntegerField()
+    adress = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
