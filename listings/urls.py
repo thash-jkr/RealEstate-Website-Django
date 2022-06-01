7b20eb3ffa74cf7584bf38a176000b601ad198ae
thash-jkr <afthashsahal123@gmail.com>
2022-06-01 19:03:03 +0530

    Created Update and Delete views

--- listings/urls.py
+++ listings/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('listings/', views.listing_list, name="listings"),
     path('listings/<int:pk>/detail/', views.listing_detail, name="details"),
+    path('listings/<int:pk>/update/', views.listing_update, name="update"),
+    path('listings/<int:pk>/delete/', views.listing_delete, name="delete"),
     path('listings/create/', views.listing_create, name="create"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -32,3 +32,21 @@ def listing_create(request):
         "form": form
     }
     return render(request, "listing_create.html", context)
+
+def listing_update(request, pk):
+    listing = Listing.objects.get(id=pk)
+    form = ListingForm(instance=listing)
+    if request.method == "POST":
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid:
+            form.save()
+            return redirect("/")
+    context = {
+        "form": form
+    }
+    return render(request, "listing_update.html", context)
+
+def listing_delete(request, pk):
+    listing = Listing.objects.get(id=pk)
+    listing.delete()
+    return redirect("/")
